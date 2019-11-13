de0b0fe75edf935440eba4dbbeaddea5f1155bdb
Hannah McEachern <hexart@ufl.edu>
2019-11-13 13:53:16 -0500

    Added CSS file

--- client/src/views/AdminList/AdminList.js
+++ client/src/views/AdminList/AdminList.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import './AdminList.css'
 
 function AdminList() {
     return (
-        <div>List of Newsletter emails in an easy to view style</div>
+        <div>
+            <h1>Newsletter List</h1>
+            <p>List of Newsletter emails in an easy to view style</p>
+            <ul>
+                <li>Email1@gmail.com</li>
+                <li>Email2@hotmail.com</li>
+                <li>Email3@aol.com</li>
+            </ul>
+
+        </div>
     );
 }
+
+export default AdminList;
