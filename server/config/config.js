a6855b790cb2b98f7ef209c88919f5ba43679f13
Ariel Barboza <a.barboza@ufl.edu>
2019-11-13 18:46:36 -0500

    make it better

--- server/config/config.js
+++ server/config/config.js
@@ -2,5 +2,5 @@ module.exports = {
     db: {
         uri: 'mongodb+srv://FaceR-Admin:Faceradtmp12345@subscriptiondb-uxxkh.mongodb.net/test?retryWrites=true&w=majority' 
     },
-    port = process.env.PORT || 80
+    port: process.env.PORT || 80
   };
