cca5c1190ff789be720614df8fb3a41d9ca297d0
Sara Gaya <sgaya16@ufl.edu>
2019-11-16 13:09:45 -0500

    added timeline component to History of Locks for about us page

--- server/config/express.js
+++ server/config/express.js
@@ -9,10 +9,10 @@ var path = require('path'),
 module.exports.init = function () {
     //connect to database
     mongoose.connect(config.db.uri, {
-         useNewUrlParser: true
+         useNewUrlParser: true, useUnifiedTopology: true
     });
-    // mongoose.set('useCreateIndex', true);
-    // mongoose.set('useFindAndModify', false);
+    mongoose.set('useCreateIndex', true);
+    mongoose.set('useFindAndModify', false);
 
     //initialize app
     var app = express();
