52465c785adcd9a181def4763cedcbacec56a5d5
Hannah McEachern <hexart@ufl.edu>
2019-12-02 14:46:28 -0500

    Admin Console Styling

--- client/src/views/AdminList/AdminList.js
+++ client/src/views/AdminList/AdminList.js
@@ -3,15 +3,29 @@ import './AdminList.css';
 
 function AdminList() {
     return (
-        <div className="list">
-            <h1>Newsletter List</h1>
-            <p>List of Newsletter emails in an easy to view style</p>
-            <ul>
-                <li>Email1@gmail.com</li>
-                <li>Email2@hotmail.com</li>
-                <li>Email3@aol.com</li>
-            </ul>
+        <div className="root-container">
+            <div className="head">
+                <h1>Admin Console</h1>
+                <p>View Newsletter and statistics</p>
+            </div>
+            <div className="row">
+                <div className="left-collumn">
+                    <div className="list">
+                        <h4>Newsletter List</h4>
+                        <p>List of Newsletter emails in an easy to view style</p>
+                        <ul>
+                            <li>Email1@gmail.com</li>
+                            <li>Email2@hotmail.com</li>
+                            <li>Email3@aol.com</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className ="right-collumn">
+                    <p>This is the right side.</p>
+                </div>
+            </div>
         </div>
+        
     );
 }
 
