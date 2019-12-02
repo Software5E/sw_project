e451b91f7cb3aac70fd3dd3f79e7c3c761dbf289
xarielx <a.barboza@ufl.edu>
2019-12-01 20:08:12 -0500

    make it better - added correct express settings

--- server/config/express.js
+++ server/config/express.js
@@ -1,69 +1,76 @@
-var path = require('path'),
-    express = require('express'),
-    mongoose = require('mongoose'),
-    config = require('./config'),
-    morgan = require('morgan'),
-    bodyParser = require('body-parser'),
-    sendMail = require('./Email/send');
-    fs = require('fs');
-    emails = require('./Email/emailList');
-    host = require('./Email/emailHost');
-    //exampleRouter = require('../routes/examples.server.routes')
-    subscriptionRouter = require('../routes/subscription.server.routes')
-    const cors = require("cors");
-    sendMail = require('./Email/send');
-    fs = require('fs');
-    emails = require('./Email/emailList');
-    host = require('./Email/emailHost');
-    //exampleRouter = require('../routes/examples.server.routes')
+var path = require("path"),
+  express = require("express"),
+  mongoose = require("mongoose"),
+  config = require("./config"),
+  morgan = require("morgan"),
+  bodyParser = require("body-parser"),
+  sendMail = require("./Email/send");
+fs = require("fs");
+emails = require("./Email/emailList");
+host = require("./Email/emailHost");
+//exampleRouter = require('../routes/examples.server.routes')
+subscriptionRouter = require("../routes/subscription.server.routes");
+const cors = require("cors");
+sendMail = require("./Email/send");
+fs = require("fs");
+emails = require("./Email/emailList");
+host = require("./Email/emailHost");
+//exampleRouter = require('../routes/examples.server.routes')
 
-module.exports.init = function () {
-    //connect to database
-    mongoose.connect(config.db.uri, {
-         useNewUrlParser: true, useUnifiedTopology: true
-    });
-    mongoose.set('useCreateIndex', true);
-    mongoose.set('useFindAndModify', false);
+module.exports.init = function() {
+  //connect to database
+  mongoose.connect(config.db.uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  mongoose.set("useCreateIndex", true);
+  mongoose.set("useFindAndModify", false);
 
-    //initialize app
-    var app = express();
+  //initialize app
+  var app = express();
 
-    //enable request logging for development debugging
-    app.use(morgan('dev'));
+  //enable request logging for development debugging
+  app.use(morgan("dev"));
 
-    app.use(cors());
+  app.use(cors());
 
-    //body parsing middleware 
-    app.use(bodyParser.json());
+  //body parsing middleware
+  app.use(bodyParser.json());
 
-    //serve client files
-    app.use(express.static(path.join(__dirname, '../../client/build')));
+  //serve client files
+  app.use(express.static(path.join(__dirname, "../../client/build")));
 
-    //add a router
-    //app.use('/api/example', exampleRouter);
-    app.put('/email',(req,res) => {
-        //Need to receive file from admin console
-        fs.readFile(path.join(__dirname,"/Email/NewsLetter/letter.html"), (err,data) => {
-            if (err) throw err;
-            host(data);
-        });
-        res.send('Emails Sent');
-    });
-    app.use('/signup', subscriptionRouter);
+  //add a router
+  //app.use('/api/example', exampleRouter);
+  app.put("/email", (req, res) => {
+    //Need to receive file from admin console
+    fs.readFile(
+      path.join(__dirname, "/Email/NewsLetter/letter.html"),
+      (err, data) => {
+        if (err) throw err;
+        host(data);
+      }
+    );
+    res.send("Emails Sent");
+  });
+  app.use("/signup", subscriptionRouter);
 
-    //app.use('/api/example', exampleRouter);
-    app.put('/email',(req,res) => {
-        //Need to receive file from admin console
-        fs.readFile(path.join(__dirname,"/Email/NewsLetter/letter.html"), (err,data) => {
-            if (err) throw err;
-            host(data);
-        });
-        res.send('Emails Sent');
-    });
-    //all other requests send to the homepage
-    app.get('*', (req,res) =>{
-        res.sendFile(path.join(__dirname + '../../../client/public/index.html'));
-    });
+  //app.use('/api/example', exampleRouter);
+  app.put("/email", (req, res) => {
+    //Need to receive file from admin console
+    fs.readFile(
+      path.join(__dirname, "/Email/NewsLetter/letter.html"),
+      (err, data) => {
+        if (err) throw err;
+        host(data);
+      }
+    );
+    res.send("Emails Sent");
+  });
+  //all other requests send to the homepage
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "../../../client/build/index.html"));
+  });
 
-    return app;
+  return app;
 };
