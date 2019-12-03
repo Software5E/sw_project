9b1ba4f112ddc2aa5445150563d3580f9a2219e6
zseibert <ZESeibert@gmail.con>
2019-12-03 02:55:39 -0500

    syntax

--- client/src/views/NewsLetter/Newsletter.js
+++ client/src/views/NewsLetter/Newsletter.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import NewsLetter from '../../components/NewsLetter/NewsLetter'
 import '../../components/NewsLetter/NewsLetter.css'
+
 function Newsletter() {
     return (
       <div class="setup">
         <NewsLetter/>
       </div>
+      
     );
 }
 
